f98d245c04d1652e3a343e21e21531b1f38ca124
Andrea Sansica <sansica.andrea99@gmail.com>
2021-01-19 17:24:03 +0100

    vf

--- index.php
+++ index.php
@@ -11,125 +11,254 @@
 <body>
 
 
+  <!-- GOAL: sulla base dell'esercizio di ieri (vedi correzione qui su slack) aggiungere i seguenti vincoli di integrita':
+  - nomi e cognomi devono essere di >3 caratteri
+  - i livelli di sicurezza devono essere [1;5] per i dipendenti e [6;10] per i boss
+  - ral employee [10.000;100.000]
+  - non puo' esistere boss senza dipendenti
+  Durante la fase di test, utilizzare il costrutto try-catch per gestire l'errore e informare l'utente -->
+
   <?php
 
-      class Persona {
-          private $name;
-          private $lastname;
-          private $age;
+  class Person {
+                    private $name;
+                    private $lastname;
+                    private $dateOfBirth;
+                    private $securyLvl;
+                    public function __construct($name, $lastname, $dateOfBirth, $securyLvl) {
+                        $this -> setName($name);
+                        $this -> setLastname($lastname);
+                        $this -> setDateOfBirth($dateOfBirth);
+                        $this -> setSecuryLvl($securyLvl);
+                    }
+                    public function getName() {
+                        return $this -> name;
+                    }
+                    public function setName($name) {
+                      if (strlen($name) < 3) {
+                        throw new Exception('The name must be at least 3 letters long');
+                     }
+                     $this -> name = $name;
+                    }
+                    public function getLastname() {
+                        return $this -> lastname;
+                    }
+                    public function setLastname($lastname) {
+                      if (strlen($lastname) < 3) {
+                        throw new Exception('The lastname must be at least 3 letters long');
 
-          public function __construct($name, $lastname, $age) {
-              $this -> setName($name);
-              $this -> setLastname($lastname);
-              $this -> setAge($age);
-          }
+                     }
+                     $this -> lastname = $lastname;
 
-          public function getName() {
-              return $this -> name;
-          }
-
-          public function setName($name) {
-              $this -> name = $name;
-          }
-
-          public function getLastname() {
-              return $this -> lastname;
-          }
-
-          public function setLastname($lastname) {
-              $this -> lastname =$lastname;
-          }
-
-          public function getAge() {
-              return $this -> age;
-          }
-
-          public function setAge($age) {
-              $this -> age = $age;
-          }
-
-
-          public function __toString() {
-              return
-                  'Nome: ' . $this -> getName() . '<br>' .
-                  'Cognome: ' . $this -> getLastname() . '<br>' .
-                  'Data di nascita: ' . $this -> getAge();
-          }
-
-      }
-
-
-      $persona1 = new Persona("Andrea", "Sansica", "11-09-1999");
-      echo $persona1 . '<br>' . '<br>';
-
-
-      class Dipendente extends Persona
-      {
-            public $salary;
-            public $job;
-
-            function __construct($name, $lastname, $age, $salary, $job)
-            {
-              parent::__construct($name, $lastname, $age);
-              $this -> setSalary($salary);
-              $this -> setJob($job);
-            }
-
-          public function getSalary() {
-             return $this -> salary;
-         }
-         public function setSalary($salary) {
-             $this -> salary = $salary;
-         }
-
-         public function getJob() {
-             return $this -> job;
-         }
-         public function setJob($job) {
-             $this -> job = $job;
-         }
-
-
-         public function __toString() {
-             return
-                 parent::__toString() . '<br>' .
-                 "Stipendio: " . $this -> getSalary() . '<br>' .
-                 "Lavoro: " . $this -> getJob();
+                    }
+                    public function getFullname() {
+                        return $this -> getName() . ' ' . $this -> getLastname();
+                    }
+                    public function getDateOfBirth() {
+                        return $this -> dateOfBirth;
+                    }
+                    public function setDateOfBirth($dateOfBirth) {
+                        $this -> dateOfBirth = $dateOfBirth;
+                    }
+                    public function getSecuryLvl() {
+                        return $this -> securyLvl;
+                    }
+                    public function setSecuryLvl($securyLvl) {
+                      $this -> securityLvl = $securityLvl;
+                    }
+                    public function __toString() {
+                        return
+                            'name: ' . $this -> getName() . '<br>'
+                            . 'lastname: ' . $this -> getLastname() . '<br>'
+                            . 'dateOfBirth: ' . $this -> getDateOfBirth() . '<br>'
+                            . 'securyLvl: ' . $this -> getSecuryLvl() . '<br>';
+                    }
                 }
-      }
-      
+                // VERSIONE 1
+                class Employee extends Person {
+                    private $ral;
+                    private $mainTask;
+                    private $idCode;
+                    private $dateOfHiring;
+                    public function __construct($name, $lastname, $dateOfBirth, $securyLvl,
+                                                $ral, $mainTask, $idCode, $dateOfHiring) {
+                        parent::__construct($name, $lastname, $dateOfBirth, $securyLvl);
+                        $this -> setRal($ral);
+                        $this -> setMainTask($mainTask);
+                        $this -> setIdCode($idCode);
+                        $this -> setDateOfHiring($dateOfHiring);
+                        $this -> setSecurityLvl($securityLvl);
+                    }
+                    public function getRal() {
+                        return $this -> $ral;
+                    }
+                    public function setRal($ral) {
+                        $this -> ral = $ral;
+                    }
+                    public function getMainTask() {
+                        return $this -> $mainTask;
+                    }
+                    public function setMainTask($mainTask) {
+                        $this -> mainTask = $mainTask;
+                    }
+                    public function getIdCode() {
+                        return $this -> $idCode;
+                    }
+                    public function setIdCode($idCode) {
+                        $this -> idCode = $idCode;
+                    }
+                    public function getDateOfHiring() {
+                        return $this -> $dateOfHiring;
+                    }
+                    public function setDateOfHiring($dateOfHiring) {
+                        $this -> dateOfHiring = $dateOfHiring;
+                    }
+                    public function getSecurityLvl() {
+                          return $this -> securityLvl;
+                      }
 
-     $dipendente1 = new Dipendente("Mario", "Rossi", "11-09-1997",  "1200$", "Operaio");
-     echo $dipendente1 . '<br>' . '<br>';
+                      public function setSecurityLvl($securityLvl) {
+                          if ($securityLvl > 1 && $securityLvl < 5) {
+                              throw new Exception("Security Level must be value from 1 to 5");
+                          }
+                          else{
+                            $this -> securityLvl = $securityLvl;
+
+                          }
+                      }
+                    public function __toString() {
+                        return parent::__toString() . '<br>'
+                            . 'ral: ' . $this -> ral . '<br>'
+                            . 'mainTask: ' . $this -> mainTask . '<br>'
+                            . 'idCode: ' . $this -> idCode . '<br>'
+                            . 'dateOfHiring: ' . $this -> dateOfHiring . '<br>';
+                    }
+                }
+                class Boss extends Employee {
+                    private $profit;
+                    private $vacancy;
+                    private $sector;
+                    private $employees;
+                    public function __construct($name, $lastname, $dateOfBirth, $securyLvl,
+                                                $ral, $mainTask, $idCode, $dateOfHiring,
+                                                $profit, $vacancy, $sector, $employees = []) {
+                        parent::__construct($name, $lastname, $dateOfBirth, $securyLvl,
+                                            $ral, $mainTask, $idCode, $dateOfHiring);
+                        $this -> setProfit($profit);
+                        $this -> setVacancy($vacancy);
+                        $this -> setSector($sector);
+                        $this -> setEmployees($employees);
+                        $this -> setSecurityLvl($securityLvl);
+                    }
+                    public function getProfit() {
+                        return $this -> profit;
+                    }
+                    public function setProfit($profit) {
+                        $this -> profit = $profit;
+                    }
+                    public function getVacancy() {
+                        return $this -> vacancy;
+                    }
+                    public function setVacancy($vacancy) {
+                        $this -> vacancy = $vacancy;
+                    }
+                    public function getSector() {
+                        return $this -> sector;
+                    }
+                    public function setSector($sector) {
+                        $this -> sector = $sector;
+                    }
+                    public function getEmployees() {
+                        return $this -> employees;
+                    }
+                    public function setEmployees($employees) {
+                        $this -> employees = $employees;
+                    }
+                    public function getSecurityLvl() {
+                          return $this -> securityLvl;
+                      }
+
+                      public function setSecurityLvl($securityLvl) {
+                          if ($securityLvl > 6 && $securityLvl < 10) {
+                              throw new Exception("Security Level must be value from 6 to 10");
+                          }
+                          $this -> securityLvl = $securityLvl;
+                      }
+                    public function __toString() {
+                        return parent::__toString() . '<br>'
+                                . 'profit: ' . $this -> getProfit() . '<br>'
+                                . 'vacancy: ' . $this -> getVacancy() . '<br>'
+                                . 'sector: ' . $this -> getSector() . '<br>'
+                                . 'employees:<br>' . $this -> getEmpsStr() . '<br>';
+                    }
+                    private function getEmpsStr() {
+                        $str = '';
+                        for ($x=0;$x<count($this -> getEmployees());$x++) {
+                            $emp = $this -> getEmployees()[$x];
+                            $fullname = $emp -> getName() . ' ' . $emp -> getLastname();
+                            $str .= ($x + 1) . ': ' . $fullname . '<br>';
+                        }
+                        return $str;
+                    }
+                }
 
 
 
-     class Boss extends Dipendente {
-      private $iva;
-
-      public function __construct($name, $lastname, $age, $salary, $job, $iva) {
-          parent::__construct($name, $lastname, $age, $salary, $job);
-          $this -> setIva($iva);
-      }
-
-      public function getIva() {
-          return $this -> iva;
-      }
-
-      public function setIva($iva) {
-          $this -> iva = $iva;
-      }
+                try {
+                  $p1 = new Person(
+                      '(p)name',
+                      '(p)lastname',
+                      '(p)dateOfBirth',
+                      '(p)securityLvl',
+                  );
+                  echo 'p1:<br>' . $p1 . '<br><br>';
+                } catch (Exception  $e) {
+                    var_dump($e); die();
+                }
 
 
-      public function __toString() {
-          return
-              parent::__toString() . '<br>' .
-              "Partita IVA: " . $this -> getIva();
-      }
-  }
 
-  $boss = new Boss("Marco", "Bianchi", "21-12-1977",  "2200$", "Direttore", "19835298725");
-  echo $boss;
+                try {
+                  $e1 = new Employee(
+                      '(e)name',
+                      '(e)lastname',
+                      '(e)dateOfBirth',
+                       10,
+                      '(e)ral',
+                      '(e)mainTask',
+                      '(e)idCode',
+                      '(e)dateOfHiring',
+                  );
+                  echo 'e1:<br>' . $e1 . '<br><br>';
+                } catch (Exception  $e) {
+                    echo $e -> getMessage(); die();
+                }
+
+
+                try {
+                  $b1 = new Boss(
+                      '(b)name',
+                      '(b)lastname',
+                      '(b)dateOfBirth',
+                      '(b)securyLvl',
+                      '(b)ral',
+                      '(b)mainTask',
+                      '(b)idCode',
+                      '(b)dateOfHiring',
+                      '(b)profit',
+                      '(b)vacancy',
+                      '(b)sector',
+                      [
+                          $e1,
+                          $e1,
+                          $e1,
+                          $e1,
+                      ]
+                  );
+                  echo 'b1:<br>' . $b1 . '<br><br>';
+                } catch (Exception  $e) {
+                  var_dump($e); die();
+                }
 
 
   ?>
